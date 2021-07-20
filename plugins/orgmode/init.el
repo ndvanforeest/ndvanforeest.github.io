9adb7909c07773441566560ab25135ebd77121e4
Nicky van Foreest <vanforeest@gmail.com>
2021-07-20 11:47:41 +0200

    Nikola auto commit.
    Nikola version: 8.1.3

--- plugins/orgmode/init.el
+++ plugins/orgmode/init.el
@@ -112,18 +112,27 @@ contextual information."
         code-html))))
 
 ;; Export images with custom link type
-(defun org-custom-link-img-url-export (path desc format)
-  (cond
-   ((eq format 'html)
-    (format "<img src=\"%s\" alt=\"%s\"/>" path desc))))
-(org-add-link-type "img-url" nil 'org-custom-link-img-url-export)
+;; (defun org-custom-link-img-url-export (path desc format)
+;;   (cond
+;;    ((eq format 'html)
+;;     (format "<img src=\"%s\" alt=\"%s\"/>" path desc))))
+;; (org-add-link-type "img-url" nil 'org-custom-link-img-url-export)
 
 ;; Export images with built-in file scheme
 (defun org-file-link-img-url-export (path desc format)
   (cond
    ((eq format 'html)
-    (format "<img src=\"/%s\" alt=\"%s\"/>" path desc))))
+    (format "<img src=\"%s\" alt=\"%s\"/>" path desc))))
 (org-add-link-type "file" nil 'org-file-link-img-url-export)
+
+;; Support for magic links (link:// scheme)
+(org-link-set-parameters
+  "link"
+  :export (lambda (path desc backend)
+             (cond
+               ((eq 'html backend)
+                (format "<a href=\"link:%s\">%s</a>"
+                        path (or desc path))))))
 
 ;; Export function used by Nikola.
 (defun nikola-html-export (infile outfile)
@@ -133,12 +142,3 @@ specified location."
     (org-macro-replace-all nikola-macro-templates)
     (org-html-export-as-html nil nil t t)
     (write-file outfile nil)))
-
-(org-link-set-parameters
- "link"
- :export (lambda (path desc backend)
-           (cond
-            ((eq 'html backend)
-             (format "<a href=\"link:%s\">%s</a>"
-                     path (or desc path)))))
-)
