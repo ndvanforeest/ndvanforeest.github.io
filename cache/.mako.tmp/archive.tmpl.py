28cb4aadc70e6532f2c0452d74f525cab875be46
Nicky van Foreest <vanforeest@gmail.com>
2023-11-01 11:51:34 +0100

    Nikola auto commit.
    Nikola version: 8.2.4

--- cache/.mako.tmp/archive.tmpl.py
+++ cache/.mako.tmp/archive.tmpl.py
@@ -5,9 +5,9 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1669377723.1472166
+_modified_time = 1698835880.9798315
 _enable_loop = True
-_template_filename = '/home/nicky/.local/lib/python3.10/site-packages/nikola/data/themes/base/templates/archive.tmpl'
+_template_filename = '/home/nicky/.direnv/python-3.11/lib/python3.11/site-packages/nikola/data/themes/base/templates/archive.tmpl'
 _template_uri = 'archive.tmpl'
 _source_encoding = 'utf-8'
 _exports = []
@@ -37,6 +37,6 @@ def render_body(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "/home/nicky/.local/lib/python3.10/site-packages/nikola/data/themes/base/templates/archive.tmpl", "uri": "archive.tmpl", "source_encoding": "utf-8", "line_map": {"27": 0, "32": 1, "38": 32}}
+{"filename": "/home/nicky/.direnv/python-3.11/lib/python3.11/site-packages/nikola/data/themes/base/templates/archive.tmpl", "uri": "archive.tmpl", "source_encoding": "utf-8", "line_map": {"27": 0, "32": 1, "38": 32}}
 __M_END_METADATA
 """
